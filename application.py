8ed1a368a63dcd0afa5aa05e64de1a4f657d1698
Caden Picard <cadenlpicard@gmail.com>
2024-02-09 14:56:08 -0500

    - Changed get_library_of_congress_terms.py to write directly to azure sql database - updated application.py to reference prebuilt table that houses keywords and their often related keywords in a pop up window on click. - Add a parameter on the input page for temperature. This is for the GPT API.

--- application.py
+++ application.py
@@ -10,15 +10,16 @@ from sqlalchemy import create_engine, text
 # Initialize Flask app
 app = Flask(__name__)
 
-# Set result size
+# Set GLOBAL 
 result_size = 10
+TEMPERATURE = .5
 
 # Initialize results and missing variables
-icpsr_result, elsst_result, mesh_result = "", "", ""
-elsst_missing, gpt_missing, mesh_missing = "", "", ""
+icpsr_result, elsst_result, loc_result = "", "", ""
+elsst_missing, gpt_missing, loc_result = "", "", ""
 
 # Initialize check flags
-check_icpsr, check_elsst, check_mesh = False, False, False
+check_icpsr, check_elsst, check_loc = False, False, False
 
 # Load environment variables and set up credentials
 load_dotenv()
@@ -39,10 +40,10 @@ connection_url = f"mssql+pyodbc://{username}:{password}@{server_name}/{database_
 engine = create_engine(connection_url, connect_args={"timeout": 30})
 
 # Define function to interact with ChatGPT
-def chatgpt(gpt_key, chat_model, prompt):
+def chatgpt(gpt_key, chat_model, prompt,TEMPERATURE):
     openai.api_key = gpt_key
     response = openai.chat.completions.create(
-        model=chat_model, messages=[{"role": "user", "content": prompt}], seed=1, temperature = .5
+        model=chat_model, messages=[{"role": "user", "content": prompt}], seed=1, temperature = TEMPERATURE
     )
     responses = response.choices[0].message.content.strip()
     gpt_list = list(responses.split(","))
@@ -78,7 +79,7 @@ def get_dictionary_terms(file_name, gpt_list, size):
                         Limit the results to {result_size} of the most relevant phrases sourced from the "Subject Terms List"
                         Do not provide any additional commentary or feedback.
                         """
-    return chatgpt(gpt_key, chat_model, list_compare_prompt)
+    return chatgpt(gpt_key, chat_model, list_compare_prompt,TEMPERATURE)
 
 
 # Define function to find missing keywords
@@ -93,8 +94,8 @@ def get_gpt_list(text_to_analyze):
               in a comma delimited list. The themes and context descriptions should be no more than 3 words long and should not have special characters.
               Do not provide any additional commentary or feedback.
               """
-    gpt_words = chatgpt(gpt_key, chat_model, passage_prompt)
-    return [word.lower() for word in gpt_words]
+    gpt_words = chatgpt(gpt_key, chat_model, passage_prompt,TEMPERATURE)
+    return [word.lower() for word in gpt_words[:10]]
 
 
 # Define function to format data as HTML table
@@ -107,14 +108,24 @@ def format_data_as_html_table(data, headers):
         table_html += f'<th style="padding: 8px; text-align: left;">{header}</th>'
     table_html += '</tr>'
 
-    for row in data:
+    for row_index, row in enumerate(data):
         table_html += '<tr>'
-        for cell in row:
+        for cell_index, cell in enumerate(row):
             if isinstance(cell, list):
-                # Convert list items into an unordered list
-                bullet_points = ''.join(f'<li>{item}</li>' for item in cell)
-                cell = f'<ul>{bullet_points}</ul>'
-            table_html += f'<td style="padding: 8px;text-align: left;">{cell}</td>'
+                # Start an unordered list for the cell content
+                cell_content = "<ul>"
+                for item in cell:
+                    if has_related_words(item):
+                        # Make the item clickable only if it's in the ICPSR column and has related words
+                        cell_content += f'<li><a href="javascript:void(0);" onclick="fetchResults(\'{item}\')" style="color: #FFCB05; text-decoration: underline;">{item}</a></li>'
+                    else:
+                        # Just display the item as a list element
+                        cell_content += f'<li>{item}</li>'
+                cell_content += "</ul>"  # Close the unordered list
+            else:
+                # Display non-list cell content directly
+                cell_content = cell
+            table_html += f'<td style="padding: 8px;text-align: left;">{cell_content}</td>'
         table_html += '</tr>'
 
     table_html += '</table>'
@@ -122,8 +133,12 @@ def format_data_as_html_table(data, headers):
 
 
 
+
+
+
+
 # Define main function to process form and generate results
-def main(param, check_icpsr, check_elsst, check_mesh):
+def main(param, check_icpsr, check_elsst, check_loc):
     gpt_list = get_gpt_list(param)
     icpsr_keywords = return_keyword_list("3")
     gpt_missing = missing_keywords(gpt_list, icpsr_keywords)
@@ -138,40 +153,76 @@ def main(param, check_icpsr, check_elsst, check_mesh):
     else:
         elsst_result = "Not analyzed"
         elsst_missing = "Not analyzed"
-    if check_mesh == True:
-        mesh_result = get_dictionary_terms("1", gpt_list, result_size)
-        mesh_missing = missing_keywords(mesh_result, icpsr_keywords)
+    if check_loc == True:
+        loc_result = get_dictionary_terms("4", gpt_list, result_size)
+        loc_missing = missing_keywords(loc_result, icpsr_keywords)
     else:
-        mesh_result = "Not analyzed"
-        mesh_missing = "Not analyzed"
+        loc_result = "Not analyzed"
+        loc_missing = "Not analyzed"
 
     data = [
         ["ChatGPT", gpt_list, gpt_missing],
         ["ICPSR", icpsr_result, ""],
         ["ELSST", elsst_result, elsst_missing],
-        ["MESH", mesh_result, mesh_missing],
+        ["LOC", loc_result, loc_missing],
     ]
 
     headers = ["Source", "Suggested", "Keywords in ICPSR"]
     result = format_data_as_html_table(data, headers)
     return result
 
+def has_related_words(word):
+    # SQL query to check if the word has related keywords
+    sql_query = "SELECT dbo.has_related_keyword(:word)"
+    
+    # Execute SQL query
+    try:
+        with engine.connect() as connection:
+            result = connection.execute(text(sql_query), {"word": word})
+            row = result.fetchone() 
+            if row and row[0] == 1:  
+                return True
+            else:
+                return False
+    except Exception as e:
+        print(f"Error checking related words for '{word}': {e}")
+        return False
+
 
 ##################################################
 ################# FLASK ROUTES####################
 ##################################################
+# Flask route for AJAX request to get query results
+@app.route('/query-results')
+def query_results():
+    word = request.args.get('word')
+    sql_query = "exec dbo.return_associated_keywords :word"
+    print(sql_query)
+    with engine.connect() as connection:
+        result = connection.execute(text(sql_query), {"word": word})
+        rows = result.fetchall()
+
+    table_html = '<table border="1" style="border-collapse: collapse; width: 100%;">'
+    table_html += '<tr><th>Associated Keyword (# of studies)</th></tr>'
+    for row in rows:
+        table_html += f'<tr><td>{row[1]}</td></tr>'
+    
+    table_html += '</table>'
+    return table_html
 
 # Route to display the form
 @app.route("/", methods=["GET", "POST"])
 def index():
+    global TEMPERATURE
     results = ""
     if request.method == "POST":
         param = request.form.get("param", "")
-        check_mesh = "mesh" in request.form
+        TEMPERATURE = float(request.form.get("temperature", 0.5))
+        check_loc = "loc" in request.form
         check_icpsr = "icpsr" in request.form
         check_elsst = "elsst" in request.form
 
-        results = main(param, check_icpsr, check_elsst, check_mesh)
+        results = main(param, check_icpsr, check_elsst, check_loc)
 
     return render_template_string(
         """
@@ -181,73 +232,203 @@ def index():
     <meta charset="UTF-8">
     <title>Retrieve Suggested Keywords</title>
     <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            background-color: #00274C; /* Blue */
-            color: #FFCB05; /* Maize */
-            margin: 0;
-            padding: 20px;
-            text-align: center;
-        }
-        .container {
-            display: flex;
-            justify-content: space-between; /* This will place the content spaced out equally */
-            height: calc(100vh - 40px); /* Subtract the padding of the body */
-        }
-        .form-container {
-            flex: 1; /* Take up half the space */
-            padding: 0 10px; /* Give some spacing */
-        }
-        .results-container {
-            flex: 1; /* Take up the other half */
-            background-color: #00274C; /* A different shade of blue */
+    body {
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        background-color: #00274C; /* U-M Blue */
+        color: #FFCB05; /* U-M Maize */
+        margin: 0;
+        padding: 0;
+        text-align: center;
+    }
+
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        padding: 20px;
+        box-sizing: border-box;
+    }
+
+    .form-container, .results-container {
+        flex-basis: 50%;
+        padding: 20px;
+        box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+        border-radius: 8px;
+        margin: 10px;
+        background: rgba(255,255,255,0.1);
+        backdrop-filter: blur(10px);
+    }
+
+    .form-container {
+        border-right: 2px solid #FFCB05;
+    }
+
+    .results-container {
+        background-color: rgba(0, 39, 76, 0.85); /* U-M Blue with transparency */
+        color: #FFCB05; /* U-M Maize */
+        overflow-y: auto;
+    }
+
+    textarea, input[type="number"] {
+        width: 100%;
+        padding: 10px;
+        margin-bottom: 20px;
+        border: 1px solid #FFCB05;
+        border-radius: 4px;
+        background-color: #FFF;
+        color: #333;
+        box-sizing: border-box;
+    }
+    .banner {
+            background-color: #FFCB05; /* U-M Maize */
+            color: #00274C; /* U-M Blue */
             padding: 10px;
-            border-radius: 5px;
-            color: #FFCB05; /* Maize */
-            height: calc(100vh - 40px); /* Full height */
-            overflow-y: auto; /* Allow scrolling if content is too long */
+            text-align: center;
+            font-weight: bold;
         }
-        textarea {
-            width: 100%; /* Take full width of the container */
-            height: 200px; /* Set a fixed height for textarea */
-            margin-bottom: 20px;
+
+        .form-container {
+            margin-top: 2vh; /* Adjusted for centering before results */
         }
-        .checkbox-group {
-            margin-bottom: 20px;
+
+        #param {
+            height: 350px; /* Larger text area */
         }
-        button {
-            background-color: #FFCB05; /* Maize */
-            color: #00274C; /* Blue */
-            padding: 10px 20px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            transition: background-color 0.3s;
+
+        #temperature {
+            width: 60px; /* Smaller input for temperature */
+            text-align: center;
+            margin-bottom:20px;
+            margin-top:20px;
         }
-        button:hover {
-            background-color: #E6B905; /* Darker Maize */
+        
+    .checkbox-group label {
+        margin-right: 20px;
+        color: #FFCB05;
+        font-weight: bold;
+    }
+
+    input[type="checkbox"] {
+        accent-color: #FFCB05;
+    }
+
+    button {
+        background-color: #FFCB05; /* U-M Maize */
+        color: #00274C; /* U-M Blue */
+        padding: 10px 20px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        font-weight: bold;
+        text-transform: uppercase;
+        box-shadow: 0 2px 4px rgba(0,0,0,0.2);
+        transition: all 0.3s ease;
+    }
+
+    button:hover, button:focus {
+        background-color: #E6B905; /* Darker Maize */
+        outline: none;
+    }
+
+    h1, h2, h3 {
+        font-weight: bold;
+    }
+
+    /* Responsive adjustments */
+    @media (max-width: 768px) {
+        .container {
+            flex-direction: column;
         }
+
+        .form-container, .results-container {
+            flex-basis: auto;
+            margin-top:2vh;
+            width: 100%;
+        }
+    
+    }
     </style>
+    <script>
+        function showPopup(data) {
+            const popup = document.createElement('div');
+            popup.style.position = 'fixed';
+            popup.style.left = '50%';
+            popup.style.top = '50%';
+            popup.style.transform = 'translate(-50%, -50%)';
+            popup.style.backgroundColor = '#00274C'; // U-M Blue
+            popup.style.color = '#FFCB05'; // U-M Maize
+            popup.style.padding = '20px';
+            popup.style.borderRadius = '8px';
+            popup.style.border = '1px solid #FFCB05'; // U-M Maize
+            popup.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.2)';
+            popup.style.zIndex = '1000';
+
+            const content = document.createElement('div');
+            content.innerHTML = data;
+            content.style.padding = '20px';
+            content.style.borderBottom = '1px solid #FFCB05'; // U-M Maize
+
+            const closeButton = document.createElement('button');
+            closeButton.textContent = 'Close';
+            closeButton.style.padding = '10px 20px';
+            closeButton.style.marginTop = '10px';
+            closeButton.style.border = 'none';
+            closeButton.style.borderRadius = '4px';
+            closeButton.style.backgroundColor = '#FFCB05'; // U-M Maize
+            closeButton.style.color = '#00274C'; // U-M Blue
+            closeButton.style.cursor = 'pointer';
+            closeButton.style.fontWeight = 'bold';
+            closeButton.style.textTransform = 'uppercase';
+            closeButton.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.2)';
+            closeButton.onmouseover = function() {
+                this.style.backgroundColor = '#E6B905'; // Darker Maize
+            };
+            closeButton.onmouseout = function() {
+                this.style.backgroundColor = '#FFCB05'; // U-M Maize
+            };
+            closeButton.onclick = function() {
+                document.body.removeChild(popup);
+            };
+
+            popup.appendChild(content);
+            popup.appendChild(closeButton);
+            document.body.appendChild(popup);
+    }
+
+
+        function fetchResults(word) {
+            fetch(`/query-results?word=${word}`)
+                .then(response => response.text())
+                .then(html => showPopup(html))
+                .catch(error => console.error('Error fetching details:', error));
+        }
+    </script>
 </head>
 <body>
+    <div class="banner">For Research Purposes Only</div>
     <div class="container">
         <div class="form-container">
             <form action="/" method="POST">
                 <label for="param"><h2>Enter Text To be Analyzed:</h2></label>
                 <textarea id="param" name="param"></textarea>
-                <h3>Check all sources that you would like to reference</h3>
                 <div class="checkbox-group">
                     <input type="checkbox" id="icpsr" name="icpsr" value="true">
                     <label for="icpsr">ICPSR</label>
-                    <input type="checkbox" id="mesh" name="mesh" value="true">
-                    <label for="mesh">MESH</label>
+                    <input type="checkbox" id="loc" name="loc" value="true">
+                    <label for="loc">Library of Congress</label>
                     <input type="checkbox" id="elsst" name="elsst" value="true">
                     <label for="elsst">ELSST</label>
+                </div>
+                <div></div>
+                <div class="temperature-input">
+                    <label for="temperature">Temperature:</label>
+                    <input type="number" id="temperature" name="temperature" min="0" max="1" step="0.01" value="0.5">
                 </div>
                 <button type="submit">Submit</button>
             </form>
         </div>
-        <div class="results-container">
+        <div class="results-container" style="display: {% if results %} block {% else %} none {% endif %};">
             {% if results %}
                 <div id="results">
                     <h2>Analysis Results:</h2>
--- getdata/get_library_of_congress_terms.py
+++ getdata/get_library_of_congress_terms.py
@@ -1,6 +1,30 @@
 
 import requests
 import csv
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine, text 
+
+# Load environment variables and set up credentials
+load_dotenv()
+key_vault_uri = os.getenv("KEY_VAULT_URI")
+credential = DefaultAzureCredential()
+secret_client = SecretClient(vault_url=key_vault_uri, credential=credential)
+library_file = './data/library_of_congress_parsed.csv'
+
+# Retrieve secrets
+gpt_key = secret_client.get_secret("gpt4-api-key").value
+chat_model = "gpt-4-1106-preview"
+server_name = secret_client.get_secret("azuresql-db-icpsrserver").value
+database_name = secret_client.get_secret("icpsr-database").value
+username = secret_client.get_secret("sql-adminusername").value
+password = secret_client.get_secret("sql-adminpassword").value
+
+# Construct the connection URL and create engine
+connection_url = f"mssql+pyodbc://{username}:{password}@{server_name}/{database_name}?driver=ODBC+Driver+17+for+SQL+Server"
+engine = create_engine(connection_url, connect_args={"timeout": 30})
 
 
 def create_url_library_of_congress(URI):
@@ -15,49 +39,35 @@ def create_url_library_of_congress(URI):
 
 def generate_api_content_library_of_congress(url):
     r = requests.get(url)
-    if r.status_code ==200:
-        data=r.json()
+    if r.status_code == 200:
+        data = r.json()
         for item in data:
-            if "@type" in item and "http://www.loc.gov/mads/rdf/v1#TopicElement"  in item["@type"]:
+            if "@type" in item and "http://www.loc.gov/mads/rdf/v1#TopicElement" in item["@type"]:
                 if "http://www.loc.gov/mads/rdf/v1#elementValue" in item:
                     value = item["http://www.loc.gov/mads/rdf/v1#elementValue"][0]["@value"]
-                    #print(value)
-                    return(value)
-                
-def write_loc_terms(output_file, uri_file):
-          
-    with open(uri_file) as csvfile:
-            csvreader = csv.reader(csvfile)
-            for row in csvreader:
-                api_content = generate_api_content_library_of_congress(create_url_library_of_congress(row))
-                if api_content != None:
-                    with open(output_file,'a', encoding = "utf-8") as f1:
-                            writer=csv.writer(f1, delimiter=',',lineterminator='\n',)
-                            writer.writerow(api_content) 
-                            
-def create_unique_file(file_name):
-    
-    file = file_name
-    # Step 1: Read the file and store unique values
-    unique_keywords = set()
-    with open(file, 'r', newline='', encoding='utf-8') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter='\t',lineterminator='\n')
-        header = next(csvreader)  # Skip the header row
-        for row in csvreader:
-            keyword = row[0].strip()
-            unique_keywords.add(keyword)
+                    if value is not None:
+                        insert_statement = text("INSERT INTO dbo.loc_keywords (keyword) VALUES (:value)")
+                        with engine.connect() as connection:
+                            transaction = connection.begin()
+                            try:
+                                result = connection.execute(insert_statement, {"value": value})
+                                transaction.commit()  # Explicitly commit the transaction
+                                if result.rowcount > 0:
+                                    print(f"Record inserted successfully. Value: {value}. Rows affected: {result.rowcount}")
+                                else:
+                                    print("No records were inserted.")
+                            except Exception as e:
+                                transaction.rollback()  # Roll back the transaction in case of error
+                                print(f"An error occurred during insertion: {e}")
+                                # Removed return statement from here to allow processing of all items
 
-    # Step 2: Overwrite the file with unique values
-    # with open(file, 'w', newline='', encoding='utf-8') as csvfile:
-    #     csvwriter = csv.writer(csvfile, delimiter=',')
-    #     csvwriter.writerow(['keyword'])  # Write the header
-    #     for keyword in sorted(unique_keywords):
-    #         csvwriter.writerow([keyword])   
-    return(unique_keywords)             
-
+                                    
 def main():
     
-    write_loc_terms('LOC_Terms.csv', 'library_of_congress_parsed.csv')
+    with open(library_file) as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            generate_api_content_library_of_congress(create_url_library_of_congress(row))
 
                   
 
