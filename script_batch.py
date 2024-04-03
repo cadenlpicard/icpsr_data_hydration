f6e6742e661b0bfe637c882143f702e5a76216d2
Caden Picard <cadenlpicard@gmail.com>
2024-04-03 09:57:26 -0400

    Created script to process batch studies and return GPT results.

--- script_batch.py
+++ script_batch.py
@@ -1,10 +1,26 @@
-import pandas as pd
+
 from openai import AzureOpenAI
+import pandas as pd
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from dotenv import load_dotenv
-import requests
 import os
+from sqlalchemy import create_engine
+import io
+import csv
+
+
+# Set GLOBAL 
+result_size = 10
+TEMPERATURE = .5
+STUDYID = 0
+
+# Initialize results and missing variables
+icpsr_result, elsst_result, loc_result = "", "", ""
+elsst_missing, gpt_missing, loc_result = "", "", ""
+
+# Initialize check flags
+check_icpsr, check_elsst, check_loc = False, False, False
 
 # Load environment variables and set up credentials
 load_dotenv()
@@ -12,39 +28,62 @@ key_vault_uri = os.getenv("KEY_VAULT_URI")
 credential = DefaultAzureCredential()
 secret_client = SecretClient(vault_url=key_vault_uri, credential=credential)
 
-# Retrieve secrets
+
+# Azure SQL - Retrieve secrets
+server_name = secret_client.get_secret("azuresql-db-icpsrserver").value
+database_name = secret_client.get_secret("icpsr-database").value
+username = secret_client.get_secret("sql-adminusername").value
+password = secret_client.get_secret("sql-adminpassword").value
+
+# Construct the connection URL and create engine
+connection_url = f"mssql+pyodbc://{username}:{password}@{server_name}/{database_name}?driver=ODBC+Driver+17+for+SQL+Server"
+engine = create_engine(connection_url, connect_args={"timeout": 30})
+# Create an in-memory stream to hold the CSV data
+proxy = io.StringIO()
+writer = csv.writer(proxy)
+
+
+# GPT - Retrieve secrets
 gpt_key = secret_client.get_secret("gpt4-api-key").value
-chat_model = "gpt-4"
-base_url = "https://api.umgpt.umich.edu/azure-openai-api" #secret_client.get_secret("openai-api-base").value
 shortcode = secret_client.get_secret("shortcode").value
-headers = {
-    "Authorization": f"Bearer {gpt_key}"
-}
+base_url = secret_client.get_secret("openai-api-base").value
+chat_model = "2023-05-15" 
+deploy_id = 'gpt-4-turbo' 
+
+#Create OpenAI client
+client = AzureOpenAI(
+    api_key=gpt_key,  
+    api_version=chat_model,
+    azure_endpoint = base_url,
+    organization = shortcode
+)
+
+
+# Define function to interact with ChatGPT
+def chatgpt(prompt,TEMPERATURE):   
+    response = client.chat.completions.create(
+    model=deploy_id, messages=[{"role": "user", "content": prompt}], seed=1, temperature = TEMPERATURE
+    )
+    
+    responses = response.choices[0].message.content.strip()
+    gpt_list = [item.strip() for item in responses.split(",")]
+    return gpt_list
+
+
+
 #Files
 input_file = './gpt_sample.csv'
 output_file = './gpt_sample_processed.csv'
 
+def get_gpt_list(text_to_analyze):
 
-# Create Azure client
-client = AzureOpenAI(
-    api_key=gpt_key,
-    api_version=chat_model, 
-    azure_endpoint=base_url,
-    organization=shortcode#,
-    #headers = headers
-)
-
-# Temperature for GPT-4 responses
-TEMPERATURE = .5
-
-# Define function to interact with ChatGPT
-def chatgpt(prompt, TEMPERATURE):
-    response = client.chat.completions.create(
-        model=chat_model, messages=[{"role": "user", "content": prompt}], seed=1, temperature=TEMPERATURE
-    )
-    responses = response.choices[0].message.content.strip()
-    gpt_list = [item.strip() for item in responses.split(",")]
-    return gpt_list
+    passage_prompt = f"""
+              Here is a passage of text: {text_to_analyze}.As an expert in social science subject terms, analyze the passage to understand the main context and themes. Limit the results to the {result_size} best of the most relevant phrases. Return them
+              in a comma delimited list. The themes and context descriptions should be no more than 3 words long and should not have special characters.
+              Do not provide any additional commentary or feedback.
+              """
+    gpt_words = chatgpt(passage_prompt,TEMPERATURE)
+    return [word.lower() for word in gpt_words[:10]]
 
 # Function to process CSV file and output results
 def process_csv(input_file, output_file):
@@ -60,10 +99,12 @@ def process_csv(input_file, output_file):
         text = row['DATA']
         
         # Generate GPT response
-        gpt_response = chatgpt(text, TEMPERATURE)
+        gpt_response = get_gpt_list(text)
+        print(gpt_response)
         
         # Append the results to the output DataFrame
         output_df = output_df.append({'study_id': study_id, 'text': text, 'gpt_response': gpt_response}, ignore_index=True)
+        #print(output_df)
     
     # Write the output DataFrame to a CSV file
     output_df.to_csv(output_file, index=False)
